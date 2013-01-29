d139e71c52865439d52e077865051e4160ded9ef
Joshua Harris <joshua@Tempest2.(none)>
2013-01-28 19:08:09 -0600

    Work on Problem 1

--- mp2grader/student.ml
+++ mp2grader/student.ml
@@ -4,16 +4,24 @@ open Mp2common
 (* TODO: complete cases for each listed expression construct.
    You will need a function to process a list of expressions. *)
 
+let rec removeExistingId lis curr = match lis with
+		h::t	->	if h = curr then t else h::(removeExistingId t curr)
+	|	[]	->	[];;
+
 let rec alldeclaredExp vars e = match e with
-          Operation(e1,_,e2) -> true
-        | Subscript(e1,e2) -> true
-        | Length(e1) -> true
-        | MethodCall(e1,_,el) -> true
-        | FieldRef(e1,_) -> true
-        | NewArray(_,e1) -> true
-        | Not(e1) -> true
-        | Id(s) -> true
-        | _ -> true ;;
+          Operation(e1,_,e2) -> (alldeclaredExp vars e1) && (alldeclaredExp vars e2)
+        | Subscript(e1,e2) -> (alldeclaredExp vars e1) && (alldeclaredExp vars e2)
+        | Length(e1) -> (alldeclaredExp vars e1)
+        | MethodCall(e1,_,el) -> (alldeclaredExp vars e1) 
+        | FieldRef(e1,_) -> alldeclaredExp vars e1
+        | NewArray(_,e1) -> alldeclaredExp vars e1
+        | Not(e1) -> alldeclaredExp vars e1
+        | Id(s) -> removeExistingId vars s
+        | _ -> if Length vars = 0 then true else false ;;
+
+let rec adeOnExpressionList vars elist = match elist with
+		h::t	->	(alldeclaredExp vars h) || (adeOnExpressionList vars t)
+	|	[]		->	false;;
 
 let rec alldeclaredSt vars st = true ;;
 
